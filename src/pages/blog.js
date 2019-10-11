219ee26d8583d9206ecd57a6a08400d5366569ca
Johnny1992 <evgeny.frontend@gmail.com>
2019-10-11 17:05:33 +0300

    added style for blog list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,33 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
 import Layout from "../components/layout"
+
+const StyledItems = styled.ol`
+  list-style: none;
+  margin: 0;
+  .post-item {
+    margin: 1rem 0;
+    a {
+      display: block;
+      background: #f4f4f4;
+      color: #000000;
+      padding: 1rem;
+      text-decoration: none;
+      &:hover {
+        background: #e4e4e4;
+      }
+    }
+    h2 {
+      margin-bottom: 0;
+    }
+    p {
+      font-size: 0.8rem;
+      color: #777777;
+      font-style: italic;
+    }
+  }
+`
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -24,10 +51,10 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <StyledItems>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li key={edge.node.frontmatter.title}>
+            <li className="post-item" key={edge.node.frontmatter.title}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
@@ -35,7 +62,7 @@ const BlogPage = () => {
             </li>
           )
         })}
-      </ol>
+      </StyledItems>
     </Layout>
   )
 }
